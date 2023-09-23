951c5578f37e09f3d1b5c9b99d74c20aac56699e
autumn maple <lepeshkinaka@mer.ci.nsu.ru>
2023-09-23 14:25:08 +0700

    карта

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "leaflet/dist/leaflet.css"
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+const position = [51.505, -0.09]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          qwe
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
