de876f03f3d619aa3bd5314d1150f77363bced4b
autumn maple <lepeshkinaka@mer.ci.nsu.ru>
2023-10-05 20:48:58 +0700

    календарь

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import "leaflet/dist/leaflet.css"
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 const position = [51.505, -0.09]
 function App() {
-  return (
+  return ( 
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          qwe
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <input type="date" id="start" name="trip-start" value="2023-10-01" min="2023-01-01" max="2023-12-31" />
+      <Popup>
+        qwe
+      </Popup>
+    </Marker>    
+    </MapContainer>  
   );
 }
 
