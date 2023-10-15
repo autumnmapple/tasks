c9dfdc27ee471de6cbcb4bccf20d4a76d3aae9c4
autumn maple <lepeshkinaka@mer.ci.nsu.ru>
2023-10-15 15:28:09 +0700

    добавление

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import "leaflet/dist/leaflet.css"
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
-const position = [51.505, -0.09]
-function App() {
-  return ( 
+import React, {useState} from "react";
+import "./App.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import ParentComponent from './components/ParentComponent'
+const position = [51.505, -0.09];
+
+const App = () => {
+  
+  return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <input type="date" id="start" name="trip-start" value="2023-10-01" min="2023-01-01" max="2023-12-31" />
-      <Popup>
-        qwe
-      </Popup>
-    </Marker>    
-    </MapContainer>  
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <ParentComponent />
+        <Popup>qwe</Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
